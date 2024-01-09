75bf5d4dce34edbfeb3fa83cb2760e294ee631d8
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2024-01-09 23:07:37 +0900

    followed original checkviz genereration

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ import sys
 import os
 from distutils.core import setup
 
-setup(name='two_class_density_scatterplot',
-      version=0.2,
-      packages=[''],
-      package_dir={'': '.'},
-      install_requires=['scipy', 'numpy', 'pandas', 'coloraide', 'matplotlib'],
-      py_modules=['two_class_density_scatterplot'])
+setup(
+    name="two_class_density_scatterplot",
+    version=0.2,
+    packages=[""],
+    package_dir={"": "."},
+    install_requires=["scipy", "numpy", "pandas", "coloraide", "matplotlib"],
+    py_modules=["two_class_density_scatterplot"],
+)
--- two_class_density_scatterplot.py
+++ two_class_density_scatterplot.py
@@ -5,18 +5,18 @@ from matplotlib import cm
 
 
 def density_and_class_ratio(X, y, normalize_density=True):
-    '''
-	inputs
-	-----
-	X: 2D positions (shape: (n_samples, 2))
-	y: 2-class labels. must be 0 or 1 (shape: (n_samples, ))
-	normalize_density: whether scaling density within 0-1
+    """
+    inputs
+    -----
+    X: 2D positions (shape: (n_samples, 2))
+    y: 2-class labels. must be 0 or 1 (shape: (n_samples, ))
+    normalize_density: whether scaling density within 0-1
 
-	outputs
-	----
-	density: density (shape: (n_samples, ))
-	class1_ratio: ratio of class1/(class0 + class1) (shape: (n_samples, ))
-	'''
+    outputs
+    ----
+    density: density (shape: (n_samples, ))
+    class1_ratio: ratio of class1/(class0 + class1) (shape: (n_samples, ))
+    """
     density = gaussian_kde(X.T)(X.T)
     order = density.argsort()
 
@@ -35,42 +35,44 @@ def density_and_class_ratio(X, y, normalize_density=True):
     return density, class1_ratio
 
 
-def gen_polar_colormap(cmap0=cm.Reds,
-                       cmap1=cm.Blues,
-                       interpolation_space='lch',
-                       interpolation_hue_selection='shorter'):
-    '''
-	inputs
-	-----
-	cmap0: matplotlib colormap corresponding to class0
-	cmap1: matplotlib colormap corresponding to class1
-	interpolation_space: color space used for interpolation. check coloraide.Color.interpolate
-	interpolation_hue_selection: a way for hue interpolation. check coloraide.Color.interpolate
+def gen_polar_colormap(
+    cmap0=cm.Reds,
+    cmap1=cm.Blues,
+    interpolation_space="lch",
+    interpolation_hue_selection="shorter",
+):
+    """
+    inputs
+    -----
+    cmap0: matplotlib colormap corresponding to class0
+    cmap1: matplotlib colormap corresponding to class1
+    interpolation_space: color space used for interpolation. check coloraide.Color.interpolate
+    interpolation_hue_selection: a way for hue interpolation. check coloraide.Color.interpolate
 
-	outputs
-	-----
-	polor_colormap: function to generate color from radius and ratio of class1
-		
-        inputs
-		-----
-		radius: decides location of each colormap
-		ratio1: decides ratio of how much the interpolation inclines to cmap1
+    outputs
+    -----
+    polor_colormap: function to generate color from radius and ratio of class1
 
-		outputs
-		-----
-		r, g, b, a: colors and alphs in ranges of 0-1
-	'''
+    inputs
+            -----
+            radius: decides location of each colormap
+            ratio1: decides ratio of how much the interpolation inclines to cmap1
+
+            outputs
+            -----
+            r, g, b, a: colors and alphs in ranges of 0-1
+    """
 
     def polor_colormap(radius, ratio1):
         r0, g0, b0, a0 = cmap0(radius)
         r1, g1, b1, a1 = cmap1(radius)
 
         ci = Color.interpolate(
-            [Color('srgb', [r0, g0, b0]),
-             Color('srgb', [r1, g1, b1])],
+            [Color("srgb", [r0, g0, b0]), Color("srgb", [r1, g1, b1])],
             space=interpolation_space,
-            out_space='srgb',
-            hue=interpolation_hue_selection)
+            out_space="srgb",
+            hue=interpolation_hue_selection,
+        )
         r, g, b, _ = ci(ratio1)
 
         a = a0 + ratio1 * (a1 - a0)
@@ -83,10 +85,10 @@ def gen_polar_colormap(cmap0=cm.Reds,
 
 
 def polar_colormap(radius, ratio1):
-    '''
+    """
     Default polar colormap used in the paper below:
     Lu et al., "Visual Analytics of Multivariate Networks with Representation Learning and Composite Variable Construction", Visual Informatics, 2023 (forthcoming).
-    
+
     inputs
     -----
     radius: decides color lightness
@@ -95,20 +97,25 @@ def polar_colormap(radius, ratio1):
     outputs
     -----
     r, g, b, a: colors and alphs in ranges of 0-1
-    '''
+    """
     return gen_polar_colormap()(radius, ratio1)
 
 
-def gen_checkviz_colormap(values1, values2):
+def gen_checkviz_colormap(
+    values1,
+    values2,
+    corner_labs=[[100, 0, 0], [65, -30, 20], [65, 30, -20], [30, 0, 0]],
+    corner_coords=[[0, 0], [0, 1], [1, 0], [1, 1]],
+    out_space="srgb-linear",
+):
     # implementation of bivariate colormap introduced in:
     # CheckViz: Sanity Check and Topological Clues for Linear and Non-Linear Mappings
     # https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1467-8659.2010.01835.x
-
-    # to implement this function, referred to:
-    # https://github.com/hj-n/snc-reliability-map/blob/master/src/components/Checkviz.js
-
+    # "(100, 0, 0) for 'white'; (65,−30, 20) for 'green'; (30, 0, 0) for 'dark grey' and (65,30,−20) for 'purple'"
     import numpy as np
+    from scipy.interpolate import LinearNDInterpolator
 
+    interpolator = LinearNDInterpolator(corner_coords, corner_labs)
     max1 = np.max(values1)
     min1 = np.min(values1)
     max2 = np.max(values2)
@@ -117,32 +124,26 @@ def gen_checkviz_colormap(values1, values2):
     def return_color(val1, val2):
         v1 = (val1 - min1) / (max1 - min1)
         v2 = (val2 - min2) / (max2 - min2)
-        va = min(1, max(v1 - v2, -1))
-        vb = min(1, max(v2 - v1, -1))
-        vl = min(1, (max(1 - (v1 + v2) / 2, 0)))
-
-        scale = 1.3
-        a = va * 30 * scale
-        b = vb * 20 * scale
-        l = vl**1.5145 * 100
-
-        r, g, b, a = Color('lab', [l, a, b]).convert('srgb')
+        lab = interpolator(v1, v2)
+        r, g, b, a = Color("lab", lab).convert(out_space)
         # coloraide returns negative value sometimes
         r, g, b, a = [max(x, 0) for x in [r, g, b, a]]
+        # coloraide returns value over 1sometimes
+        r, g, b, a = [min(x, 1) for x in [r, g, b, a]]
 
         return r, g, b, a
 
     return return_color
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import numpy as np
     import pandas as pd
     import matplotlib.pyplot as plt
 
-    df = pd.read_csv('./data/sample_data.csv')
-    y = np.array(df['label'])
-    X = np.array(df[['x_pos', 'y_pos']])
+    df = pd.read_csv("./data/sample_data.csv")
+    y = np.array(df["label"])
+    X = np.array(df[["x_pos", "y_pos"]])
 
     # compute density and class 1 ratio
     density, class1_ratio = density_and_class_ratio(X, y)
@@ -151,57 +152,67 @@ if __name__ == '__main__':
 
     # checkviz colormap just for comparison with polor colormap
     cvmap = gen_checkviz_colormap(density, class1_ratio)
+    # # example using red and blue (high contrast)
+    # cvmap = gen_checkviz_colormap(
+    #     density,
+    #     class1_ratio,
+    #     corner_labs=[[100, 0, 0], [68, -14, -27], [63, 55, 47], [30, 0, 0]],
+    # )
+    # # example using purple and green for different way from checkviz
+    # cvmap = gen_checkviz_colormap(
+    #     density,
+    #     class1_ratio,
+    #     corner_labs=[
+    #         [100, -30, 20],
+    #         [100, 30, -20],
+    #         [0, -30, 20],
+    #         [0, 30, -20],
+    #     ],
+    #     out_space="srgb",
+    # )
 
     fig, axs = plt.subplots(ncols=4, figsize=(12.5, 3.5))
-
     # ordinary two-class colored scatteplot
     order = np.arange(X.shape[0])
     np.random.shuffle(order)
-    colors = np.array(['#fb694a'] * X.shape[0])
-    colors[y == 1] = '#6aaed6'
+    colors = np.array(["#fb694a"] * X.shape[0])
+    colors[y == 1] = "#6aaed6"
 
-    axs[0].scatter(X[order, 0],
-                   X[order, 1],
-                   c=colors,
-                   edgecolors='#888888',
-                   lw=0.2)
-    axs[0].set_title('two-class scatter', fontsize=9)
+    axs[0].scatter(X[order, 0], X[order, 1], c=colors, edgecolors="#888888", lw=0.2)
+    axs[0].set_title("two-class scatter", fontsize=9)
 
     # ordinary single class density scatterplot
     order = np.argsort(density)
-    axs[1].scatter(X[order, 0],
-                   X[order, 1],
-                   c=density[order],
-                   edgecolors='#888888',
-                   lw=0.2,
-                   zorder=10,
-                   cmap='Greens')
-    axs[1].set_title('density scatter', fontsize=9)
+    axs[1].scatter(
+        X[order, 0],
+        X[order, 1],
+        c=density[order],
+        edgecolors="#888888",
+        lw=0.2,
+        zorder=10,
+        cmap="Greens",
+    )
+    axs[1].set_title("density scatter", fontsize=9)
 
     # two-class densiy scatteplot but using existing colormap (checkcviz colormap)
-    colors = np.array([
-        cvmap(radius, ratio1) for radius, ratio1 in zip(density, class1_ratio)
-    ])
+    colors = np.array(
+        [cvmap(radius, ratio1) for radius, ratio1 in zip(density, class1_ratio)]
+    )
     order = np.argsort(density)
-    axs[2].scatter(X[order, 0],
-                   X[order, 1],
-                   c=colors[order],
-                   edgecolors='#888888',
-                   lw=0.2)
-    axs[2].set_title('two-class density scatter (w/ CheckViz colormap)',
-                     fontsize=9)
+    axs[2].scatter(
+        X[order, 0], X[order, 1], c=colors[order], edgecolors="#888888", lw=0.2
+    )
+    axs[2].set_title("two-class density scatter (w/ CheckViz colormap)", fontsize=9)
 
     # two-class densiy scatteplot but using polar colormap
-    colors = np.array([
-        pcmap(radius, ratio1) for radius, ratio1 in zip(density, class1_ratio)
-    ])
+    colors = np.array(
+        [pcmap(radius, ratio1) for radius, ratio1 in zip(density, class1_ratio)]
+    )
     order = np.argsort(density)
-    axs[3].scatter(X[order, 0],
-                   X[order, 1],
-                   c=colors[order],
-                   edgecolors='#888888',
-                   lw=0.2)
-    axs[3].set_title('two-class density scatter (w/ new colormap)', fontsize=9)
+    axs[3].scatter(
+        X[order, 0], X[order, 1], c=colors[order], edgecolors="#888888", lw=0.2
+    )
+    axs[3].set_title("two-class density scatter (w/ new colormap)", fontsize=9)
 
     for ax in axs:
         ax.set(xticks=[])
@@ -217,17 +228,18 @@ if __name__ == '__main__':
 
     n = 200
     angle = 60
+    projection = "polar"  # "polar" or None
+    cmap = pcmap  # e.g., pcmap, cvmap
 
     fig, _ = plt.subplots(1, 1)
-    c_density, c_ratio = np.meshgrid(np.linspace(0, 1, n),
-                                     np.linspace(0, 1, n))
+    c_density, c_ratio = np.meshgrid(np.linspace(0, 1, n), np.linspace(0, 1, n))
     theta = c_ratio * (angle / 180) * np.pi - (angle / 360) * np.pi
 
     # make 1D colormap used to map location to a polar colormap
     cmap_colors = []
     for i in range(n):
         for j in range(n):
-            cmap_colors.append(pcmap(c_density[i, j], c_ratio[i, j]))
+            cmap_colors.append(cmap(c_density[i, j], c_ratio[i, j]))
     newcmp = ListedColormap(cmap_colors)
 
     z = np.zeros((n, n))
@@ -235,11 +247,11 @@ if __name__ == '__main__':
         for j in range(n):
             z[i, j] = n * i + j
 
-    ax = plt.subplot(projection='polar')
+    ax = plt.subplot(projection=projection)
     ax.grid(False)
-    plt.pcolormesh(theta, c_density, z, cmap=newcmp, shading='auto')
+    plt.pcolormesh(theta, c_density, z, cmap=newcmp, shading="auto")
     plt.xticks([])
     plt.yticks([])
-    ax.axis('off')
+    ax.axis("off")
     # plt.savefig('./images/polar_colormap.png', transparent=True, dpi=300)
     plt.show()
